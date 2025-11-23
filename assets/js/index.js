4e7e9f1c263bcc0e02fa93a72e03cb1f27dcec4b
CaioVilarouca <caiovilarouca@gmail.com>
2025-11-23 01:05:41 -0300

    Services

--- assets/js/index.js
+++ assets/js/index.js
@@ -24,3 +24,27 @@ function linkAction() {
 
 navLink.forEach(n => n.addEventListener('click', linkAction))
 /* / Remove mobile */
+
+/* Services modal */
+const modalViews = document.querySelectorAll('.services-modal'),
+    modalBtns = document.querySelectorAll('.services-button'),
+    modalClose = document.querySelectorAll('.services-modal-close ')
+
+let modal = function(modalClick) {
+    modalViews[modalClick].classList.add('active-modal');
+}
+
+modalBtns.forEach((modalBtn,i) => {
+    modalBtn.addEventListener('click', () => {
+        modal(i)
+    })
+})
+
+modalClose.forEach((modalClose) => {
+    modalClose.addEventListener('click', () => {
+        modalViews.forEach((modalView) => {
+            modalView.classList.remove('active-modal')
+        })
+    })
+})
+/* / Services modal */
